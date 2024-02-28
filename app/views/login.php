d6a28235c79a4be4460b40bc07b82a48a489c263
willianbortolini <willian.borto@gmail.com>
2024-02-28 20:37:41 -0300

    s

--- app/views/login.php
+++ app/views/login.php
@@ -10,7 +10,7 @@
 
     <div class="modal-content rounded-4 shadow">
       <div class="modal-header p-5 pb-4 border-bottom-0">
-        <h1 class="fw-bold mb-0 fs-2">Bem vindo de volta!</h1>
+        <h1 class="fw-bold mb-0 fs-2">Bem vindo de volta!sss</h1>
         <a href="javascript:void(0);" onclick="window.history.back();" class="btn-close" aria-label="Close"></a>
 
       </div>
