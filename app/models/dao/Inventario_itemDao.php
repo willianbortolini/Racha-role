240a49392979c2fb82cbf3a579edba6686e66c0b
willianbortolini <willian.borto@gmail.com>
2024-02-29 20:37:46 -0300

    S

--- app/models/dao/Inventario_itemDao.php
+++ app/models/dao/Inventario_itemDao.php
@@ -8,7 +8,8 @@ class Inventario_itemDao extends Model
         $sql = "SELECT ean13 code, COUNT(quantidade) quantity "
                 . "FROM inventario_item " 
                 . "WHERE inventario_id = :inventario_id "
-                . "GROUP BY ean13 ";
+                . "GROUP BY ean13 "
+                . "ORDER BY COUNT(quantidade) DESC";
         $stmt = $this->db->prepare($sql);
         $stmt->bindValue(":inventario_id", $inventarios_id);      
         $stmt->execute();
