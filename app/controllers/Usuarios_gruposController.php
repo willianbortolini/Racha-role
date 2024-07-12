01cae062b7f379b03854f12469bee78573ca65cf
willianbortolini <willian.borto@gmail.com>
2024-07-11 21:42:19 -0300

    s

--- app/controllers/Usuarios_gruposController.php
+++ app/controllers/Usuarios_gruposController.php
@@ -5,13 +5,21 @@ namespace app\controllers;
 use app\core\Controller;
 use app\util\UtilService;
 use app\models\service\Usuarios_gruposService;
+use app\models\service\Service;
 
 class Usuarios_gruposController extends Controller
 {
+    private $tabela = "usuarios_grupos";
+    private $campo = "usuarios_grupos_id";
 
     public function __construct()
     {
         UtilService::validaUsuario();
+    }
+
+    public function usuariosDoGrupo($id)
+    {
+        echo json_encode(Service::get($this->tabela, "grupos_id", $id, true));
     }
 
 }
