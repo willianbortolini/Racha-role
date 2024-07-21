639e387f07d14772bdf493ed51ec1ea96d35deb0
willianbortolini <willian.borto@gmail.com>
2024-07-21 16:06:39 -0300

    s

--- service-worker.js
+++ service-worker.js
@@ -47,23 +47,46 @@ self.addEventListener('fetch', (event) => {
 });
 
 self.addEventListener('push', function(event) {
-  const data = event.data.json();
+  let data = {};
+  if (event.data) {
+    data = event.data.json();
+  }
   console.log('Push event received:', data);
 
   const options = {
-    body: data.body,
-    icon: '/path/to/icon.png', // Atualize com o caminho correto para o ícone
-    badge: '/path/to/badge.png' // Atualize com o caminho correto para o badge
+    body: data.body || 'Você tem uma nova mensagem!',
+    icon: data.icon || '/path/to/icon.png', // Atualize com o caminho correto para o ícone
+    badge: data.badge || '/path/to/badge.png', // Atualize com o caminho correto para o badge
+    data: {
+      url: data.url || '/'
+    }
   };
 
   event.waitUntil(
-    self.registration.showNotification(data.title, options)
+    self.registration.showNotification(data.title || 'Nova Notificação', options)
   );
 });
 
 self.addEventListener('notificationclick', function(event) {
   event.notification.close();
+  const urlToOpen = event.notification.data.url;
+
   event.waitUntil(
-    clients.openWindow(event.notification.data.url)
+    clients.matchAll({
+      type: 'window',
+      includeUncontrolled: true
+    }).then(windowClients => {
+      // Verifica se a URL já está aberta e foca nela
+      for (let i = 0; i < windowClients.length; i++) {
+        const client = windowClients[i];
+        if (client.url === urlToOpen && 'focus' in client) {
+          return client.focus();
+        }
+      }
+      // Abre uma nova janela se a URL não estiver aberta
+      if (clients.openWindow) {
+        return clients.openWindow(urlToOpen);
+      }
+    })
   );
 });
