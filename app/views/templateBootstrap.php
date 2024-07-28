7f5580d801d54a7b93bf74b6c1b7f300c106f416
willianbortolini <willian.borto@gmail.com>
2024-07-28 18:30:44 -0300

    s

--- app/views/templateBootstrap.php
+++ app/views/templateBootstrap.php
@@ -26,7 +26,6 @@
     var caminhoRetornoDelete;
   </script>
 
-  <script src="<?php echo URL_BASE  . "assets/js/jquery-3.7.0.js" ?>"></script>
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
   <link rel="stylesheet" type="text/css" href="<?php echo URL_BASE  . "assets/css2/jquery.dataTables.min.css" ?>">
   <link rel="stylesheet" href="<?php echo URL_BASE  . "assets/css2/bootstrap.min.css" ?>">
