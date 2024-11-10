a163a3893e6422a50fde66871513ebd4e3142e3c
willianbortolini <willian.borto@gmail.com>
2024-11-10 19:13:51 -0300

    s

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -12,26 +12,28 @@ class HomeController extends Controller
     private $usuario;
     public function __construct()
     {
-        if (!isset($_SESSION['id'])) {
-            $dados["view"] = "Home/Index";
-            $this->load("templateBootstrap", $dados);
-        }
+        
     }
 
     public function index()
     {
-        if (isset($_SESSION['group_id'])) {
-            $usuarios_grupos = new \stdClass();
-            $usuarios_grupos->usuarios_grupos_id = 0;
-            $usuarios_grupos->grupos_id = $_SESSION['group_id'];
-            $participantes = [$_SESSION['id']];
-            Usuarios_gruposService::salvar($usuarios_grupos, $participantes);
-
-            unset($_SESSION['group_id']);
-            Flash::limpaMsg();
-            $this->redirect(URL_BASE . "grupos/Home");
+        if (!isset($_SESSION['id'])) {
+            $dados["view"] = "Home/Index";
+            $this->load("templateBootstrap", $dados);
         }else{
-            $this->redirect(URL_BASE . "amigos/Home");
+            if (isset($_SESSION['group_id'])) {
+                $usuarios_grupos = new \stdClass();
+                $usuarios_grupos->usuarios_grupos_id = 0;
+                $usuarios_grupos->grupos_id = $_SESSION['group_id'];
+                $participantes = [$_SESSION['id']];
+                Usuarios_gruposService::salvar($usuarios_grupos, $participantes);
+    
+                unset($_SESSION['group_id']);
+                Flash::limpaMsg();
+                $this->redirect(URL_BASE . "grupos/Home");
+            }else{
+                $this->redirect(URL_BASE . "amigos/Home");
+            }  
         }
     }
 
